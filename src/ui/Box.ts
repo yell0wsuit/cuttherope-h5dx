76411839e77f28f91d7bb0f99d013bf52189e085
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-12-20 21:28:13 +0700

    make the number star to unlock change with language

--- src/ui/Box.ts
+++ src/ui/Box.ts
@@ -92,6 +92,8 @@ class Box {
     textRendered: boolean;
     renderText: () => void;
     reqImg: HTMLImageElement;
+    reqStars: number;
+    renderReqStars: () => void;
     omNomImg: HTMLImageElement;
     lockImg: HTMLImageElement;
     starImg: HTMLImageElement;
@@ -145,7 +147,19 @@ class Box {
             PubSub.subscribe(PubSub.ChannelId.LanguageChanged, this.renderText)
         );
 
-        this.reqImg = Text.drawBig({ text: String(reqstars), scaleToUI: true }) as HTMLImageElement;
+        this.reqStars = reqstars;
+        this.reqImg = new Image();
+        this.renderReqStars = () => {
+            this.reqImg = Text.drawBig({
+                text: String(this.reqStars),
+                scaleToUI: true,
+            }) as HTMLImageElement;
+        };
+        this.renderReqStars();
+
+        this.pubSubSubscriptions.push(
+            PubSub.subscribe(PubSub.ChannelId.LanguageChanged, this.renderReqStars)
+        );
 
         this.omNomImg = new Image();
         this.omNomImg.src = `${platform.uiImageBaseUrl}box_omnom.webp`;
